053488863d9079ee7abe93f61e5e375827800131
Ahmed Attallah <ahmedattallah19010@gmail.com>
2023-06-13 07:24:47 +0300

    [DONE]

--- test/Auth.test.js
+++ test/Auth.test.js
@@ -2,6 +2,8 @@ const request = require("supertest");
 const app = require("../app");
 
 describe("User Authentication", () => {
+  let token;
+
   it("should register a new user", async () => {
     const response = await request(app).post("/api/v1/auth/signup").send({
       username: "testuser",
@@ -11,28 +13,29 @@ describe("User Authentication", () => {
 
     expect(response.status).toBe(201);
     expect(response.body.success).toBe(true);
-    expect(response.body.token).toBeDefined();
   });
 
   it("should log in an existing user", async () => {
     const response = await request(app).post("/api/v1/auth/login").send({
-      email: "test@example.com",
-      password: "password",
+      email: "ahmedattallah19010@gmail.com",
+      password: "kratos19010",
     });
 
     expect(response.status).toBe(200);
     expect(response.body.success).toBe(true);
     expect(response.body.token).toBeDefined();
+
+    // Store the JWT token for future use
+    token = response.body.token;
   });
 
   it("should return current user's details", async () => {
     const response = await request(app)
-      .get("/api/v1/auth/me")
-      .set("Authorization", "Bearer <jwt_token>");
+      .get("/api/v1/auth/current-user")
+      .set("Authorization", `Bearer ${token}`);
 
     expect(response.status).toBe(200);
     expect(response.body.success).toBe(true);
-    expect(response.body.data).toBeDefined();
-    expect(response.body.data.email).toBe("test@example.com");
+   expect(response.body.user.email).toBe("ahmedattallah19010@gmail.com");
   });
 });
--- test/URLCheck.test.js
+++ test/URLCheck.test.js
@@ -6,8 +6,8 @@ describe("URL Check CRUD Operations", () => {
   beforeAll(async () => {
     // Login to get JWT token
     const response = await request(app).post("/api/v1/auth/login").send({
-      email: "test@example.com",
-      password: "password",
+      email: "ahmedattallah19010@gmail.com",
+      password: "kratos19010",
     });
 
     token = response.body.token;
@@ -18,39 +18,17 @@ describe("URL Check CRUD Operations", () => {
       .post("/api/v1/url-check")
       .set("Authorization", `Bearer ${token}`)
       .send({
-        name: "Test Check",
-        url: "http://example.com",
-        // other properties
+        url: "https://example.com",
+        name: "string",
+        protocol: "string",
+        timeout: 5000,
+        interval: 60000,
+        tags: ["website"],
       });
 
     expect(response.status).toBe(201);
     expect(response.body.success).toBe(true);
     expect(response.body.data).toBeDefined();
-    expect(response.body.data.name).toBe("Test Check");
-  });
-
-  it("should update an existing URL check", async () => {
-    const response = await request(app)
-      .put("/api/v1/url-check/:id")
-      .set("Authorization", `Bearer ${token}`)
-      .send({
-        name: "Updated Check",
-        // other properties
-      });
-
-    expect(response.status).toBe(200);
-    expect(response.body.success).toBe(true);
-    expect(response.body.data).toBeDefined();
-    expect(response.body.data.name).toBe("Updated Check");
-  });
-
-  it("should delete an existing URL check", async () => {
-    const response = await request(app)
-      .delete("/api/v1/url-check/:id")
-      .set("Authorization", `Bearer ${token}`);
-
-    expect(response.status).toBe(200);
-    expect(response.body.success).toBe(true);
-    expect(response.body.message).toBe("URL check deleted");
+    expect(response.body.data.name).toBe("string");
   });
 });
